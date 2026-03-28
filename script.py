6629471c032c106c42b3acdc3b021fb63922b9fe
marionmorel <marion.mrl@live.fr>
2026-03-28 19:26:07 +0000

    Tasks 1 - 5

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-import csv
+import pandas as pd
+
+# Task 1
+inventory = pd.read_csv('inventory.csv')
+
+# Task 2
+print(inventory.head(10))
+
+# Task 3
+staten_island = inventory.iloc[0:10]
+
+# Task 4
+product_request = staten_island['product_description']
+
+# Task 5
+seed_request = inventory[(inventory['location'] == 'Brooklyn') & (inventory['product_type'] == 'seeds')]
