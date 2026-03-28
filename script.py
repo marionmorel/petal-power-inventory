1b46252eaddda0d126f28fc45d6210e4cd879f01
marionmorel <marion.mrl@live.fr>
2026-03-28 19:32:54 +0000

    Tasks 6 - 9 and project end

--- script.py
+++ script.py
@@ -14,3 +14,17 @@ product_request = staten_island['product_description']
 
 # Task 5
 seed_request = inventory[(inventory['location'] == 'Brooklyn') & (inventory['product_type'] == 'seeds')]
+
+# Task 6
+inventory['in_stock'] = inventory.quantity.apply(lambda x: True if x > 0 else False)
+
+# Task 7
+inventory['total_walue'] = inventory.price * inventory.quantity
+
+# Task 8
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+
+# Task 9
+inventory['full_description'] = inventory.apply(combine_lambda, axis=1)
